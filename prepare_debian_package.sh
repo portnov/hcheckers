3409cdcb688ca3669cbf98bc978d77073a45b856
Ilya Portnov <portnov@bk.ru>
2019-03-09 14:00:02 +0500

    On debian package for server part.
    refs #23.

---
+++ prepare_debian_package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+TARGET=hcheckersd_0.1.0.0.orig.tar.xz 
+tar -v -cJ --exclude-vcs --exclude='*.log' --exclude='*.build' --exclude=./.stack-work --exclude=./debian --exclude=./docker --exclude=./csv --exclude=./games --exclude=./python --exclude=./test --exclude='*.tar.xz' -f ../$TARGET .
+
+LOCAL_INSTALL_ROOT=$(stack path --local-install-root)
+LOCAL_INSTALL_ROOT=$(realpath --relative-to=$PWD $LOCAL_INSTALL_ROOT)
+echo "$LOCAL_INSTALL_ROOT/bin/hcheckersd usr/bin" > debian/hcheckersd.install
+
+debuild -uc -us
+
