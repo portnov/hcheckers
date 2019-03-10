1cbeb13375a0cdb9839eb5639734d8245d37a9bf
Ilya Portnov <portnov@iportnov.ru>
2019-03-10 11:19:57 +0500

    Minor fix.

--- docker/build-ubuntu-package.sh
+++ docker/build-ubuntu-package.sh
@@ -3,7 +3,7 @@ set -e
 
 SRC=$(realpath $PWD/..)
 
-docker build -t hcheckers-ubuntu -f Dockerfile.ubuntu
+docker build -t hcheckers-ubuntu -f Dockerfile.ubuntu .
 
 docker run --name hcheckers-ubuntu --rm -v $(pwd)/target:/dst -v $(pwd)/stack:/root/.stack -v $SRC:/src hcheckers-ubuntu
 
