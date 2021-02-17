4bbc6b6a0859e7994ec6157806694635c504d0d7
Ilya Portnov <portnov@bk.ru>
2021-02-17 21:24:20 +0500

    Update script.

--- prepare_debian_package.sh
+++ prepare_debian_package.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 TARGET=hcheckersd_0.1.0.0.orig.tar.xz 
-tar -v -cJ --exclude-vcs --exclude='*.log' --exclude='*.build' --exclude=./work --exclude=./.stack-work --exclude=./debian --exclude=./docker --exclude=./csv --exclude=./games --exclude=./python --exclude=./test --exclude='*.tar.xz' -f ../$TARGET .
+tar -v -cJ --exclude-vcs --exclude=./ai --exclude='*.json' --exclude='*.log*' --exclude='*.hp' --exclude='*.eventlog' --exclude='*.prof' --exclude='*.build' --exclude=./work --exclude=./.stack-work --exclude=./debian --exclude=./docker --exclude=./csv --exclude=./games --exclude=./python --exclude=./test --exclude='*.tar.xz' -f ../$TARGET .
 
 LOCAL_INSTALL_ROOT=$(stack path --local-install-root --allow-different-user)
 LOCAL_INSTALL_ROOT=$(realpath --relative-to=$PWD $LOCAL_INSTALL_ROOT)
