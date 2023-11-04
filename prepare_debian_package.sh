735ec792223092a4d1b965896e2229e754eceee1
Ilya V. Portnov <portnov@bk.ru>
2023-11-04 22:16:43 +0500

    on debian packaging

--- prepare_debian_package.sh
+++ prepare_debian_package.sh
@@ -4,11 +4,13 @@ set -x
 
 VERSION=2023.10
 TARGET=hcheckersd_$VERSION.orig.tar.xz 
-tar -v -cJ --exclude-vcs --exclude=./ai --exclude='*.json' --exclude='*.log*' --exclude='*.hp' --exclude='*.eventlog' --exclude='*.prof' --exclude='*.build' --exclude=./work --exclude=./.stack-work --exclude=./debian --exclude=./docker --exclude=./csv --exclude=./games --exclude=./python --exclude=./test --exclude='*.tar.xz' -f ../$TARGET .
+tar -v -cJ --exclude-vcs --exclude=./ai --exclude='*.json' --exclude='*.log*' --exclude='*.hp' --exclude='*.eventlog' --exclude='*.prof' --exclude='*.build' --exclude=./work --exclude=./.stack-work --exclude=./debian --exclude=./docker --exclude=./csv --exclude=./games --exclude=./python --exclude=./test --exclude=./genetics --exclude=./pdn_examples --exclude='*.tar.xz' -f ../$TARGET .
 
 LOCAL_INSTALL_ROOT=$(stack path --local-install-root --allow-different-user)
 LOCAL_INSTALL_ROOT=$(realpath --relative-to=$PWD $LOCAL_INSTALL_ROOT)
 echo "$LOCAL_INSTALL_ROOT/bin/hcheckersd usr/bin" > debian/hcheckersd.install
+echo "initial_positions usr/share/hcheckers" >> debian/hcheckersd.install
+echo "ai_settings/* etc/hcheckers/ai" >> debian/hcheckersd.install
 
 debuild -uc -us
 
